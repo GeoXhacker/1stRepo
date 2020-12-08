fdb1ecc0db2182c697c96e77b01e8ef6a9d3f5cf
Mukisa Geofrey <mukisageophrey@gmail.com>
2020-12-08 18:40:44 +0000

    removed http server

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// const http = require('http');
 
-// const requestListener = function (req, res) {
-//   res.writeHead(200);
-//   res.end('Hello, World!');
-// }
-
-// const server = http.createServer(requestListener);
-
-// server.listen(8080, () => {
-//     console.log('Server is listening')
-// });
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
